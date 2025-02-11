1e3d98f1abd7e2c45e4964b281eb8dc0cd0ca556
Eugene Vilder <vilder.eugene@gmail.com>
2025-02-10 23:14:28 -0800

    Add test and setup Jet

---
+++ src/Hooks/useGetCountries.test.ts
@@ -0,0 +1,56 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useGetCountries } from './useGetCountries';
+import { useQuery } from '@apollo/client';
+
+// Mock the useQuery hook from Apollo Client
+jest.mock('@apollo/client', () => ({
+  useQuery: jest.fn(),
+}));
+
+describe('useGetCountries', () => {
+  it('should return loading true when query is loading', () => {
+    // Mock the useQuery to simulate loading state
+    (useQuery as jest.Mock).mockReturnValue({ loading: true, error: undefined, data: [] });
+
+    const { result } = renderHook(() => useGetCountries());
+
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toEqual([]);
+    expect(result.current.error).toBeUndefined();
+  });
+
+  it('should return error when query fails', () => {
+    const mockError = new Error('Network Error');
+    // Mock the useQuery to simulate error state
+    (useQuery as jest.Mock).mockReturnValue({ loading: false, error: mockError, data: [] });
+
+    const { result } = renderHook(() => useGetCountries());
+
+    expect(result.current.loading).toBe(false);
+    expect(result.current.error).toBe(mockError);
+    expect(result.current.data).toEqual([]);
+  });
+
+  it('should return data when query succeeds', () => {
+    const mockData = {
+      countries: [{"capital": "Washington, D.C.", "code": "US", "currency": "USD", "name": "United States", "native": "English"}, {"capital": "Ottawa", "code": "CA", "currency": "CAD", "name": null}],
+    };
+    // Mock the useQuery to simulate successful query with data
+    (useQuery as jest.Mock).mockReturnValue({ loading: false, error: undefined, data: mockData });
+
+    const { result } = renderHook(() => useGetCountries());
+
+    expect(result.current.loading).toBe(false);
+    expect(result.current.error).toBeUndefined();
+    expect(result.current.data).toEqual(mockData);
+  });
+
+  it('should return empty array if no countries are returned', () => {
+    // Mock the useQuery to simulate no data case
+    (useQuery as jest.Mock).mockReturnValue({ loading: false, error: undefined, data: { countries: [] } });
+
+    const { result } = renderHook(() => useGetCountries());
+
+    expect(result.current.data).toEqual({"countries": []});
+  });
+});
--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -6,9 +6,15 @@ import Layout from '../Components/Layout';
 const Info = () => {
   return (
     <Layout>
-      <Typography variant="h5">Information Page</Typography>
+      <Typography variant="h5">Eugene's Information</Typography>
       <Typography variant="body1">
-        This is a simple example of using Apollo Client and Material-UI.
+        <strong>Full Name:</strong> Eugene Vilder
+        <br />
+        <strong>Description:</strong> Home assignment for the Monte Carlo project, designed to showcase the integration of various technologies and tools for efficient workflows and systems. The assignment includes developing web applications with React, Apollo GraphQL, and Material-UI, tailored for practical use cases and modern user experiences.
+        <br />
+        <strong>Email:</strong> vilder.eugene@gmail.com
+        <br />
+        <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/vildereugene/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/vildereugene/</a>
       </Typography>
       <Link to="/">
         <Button variant="contained">Back to Home</Button>
